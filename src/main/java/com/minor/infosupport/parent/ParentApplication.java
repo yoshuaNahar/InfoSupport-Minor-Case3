26b832fb240129576db7a77c4d23a3b39f18163d
NoahKr <noahkroon1997@gmail.com>
2017-12-14 13:32:58 +0100

    test parent project

---
+++ src/main/java/com/minor/infosupport/parent/ParentApplication.java
@@ -0,0 +1,12 @@
+package com.minor.infosupport.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentApplication.class, args);
+	}
+}
