be8c24ab29a925297a2733d36317787e52683938
Yoshua <yosh.nahar@gmail.com>
2017-12-18 15:09:32 +0100

    Add test

---
+++ webwinkel/src/test/java/nl/kantilever/webwinkel/controller/HelloWorldControllerTest.java
@@ -0,0 +1,19 @@
+package nl.kantilever.webwinkel.controller;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class HelloWorldControllerTest {
+
+  private HelloWorldController helloWorld = new HelloWorldController();
+
+  @Test
+  public void testMultiply() {
+    int answer = helloWorld.multiply(5, 4);
+
+    assertThat(answer, is(20));
+  }
+
+}
