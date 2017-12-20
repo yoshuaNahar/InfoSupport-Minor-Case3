ae62e341b3cad566351ae882ed494a2920e90e20
NoahKr <noahkroon1997@gmail.com>
2017-12-20 15:26:21 +0100

    fix mutation test

--- bestellingservice/src/test/java/PlaceHolderTest.java
+++ bestellingservice/src/test/java/PlaceHolderTest.java
@@ -1,13 +1,24 @@
+import nl.kantilever.bestellingservice.controllers.TestController;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 
 public class PlaceHolderTest {
 
+  TestController testController;
+
+  @Before
+  public void setup() {
+    this.testController = testController;
+  }
+
   @Test
   public void test() {
-    Assert.assertThat(20, is(4*5));
+    int result = testController.multiply(4, 5);
+
+    Assert.assertThat(result, is(20));
   }
 
 }
