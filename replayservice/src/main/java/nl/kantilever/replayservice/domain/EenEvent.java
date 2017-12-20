c88f81231443f4c0f75c1fa9cdae0ea65b02ea92
NoahKr <noahkroon1997@gmail.com>
2017-12-20 10:21:27 +0100

    Parsing stuff replayservice

--- replayservice/src/main/java/nl/kantilever/replayservice/domain/EenEvent.java
+++ replayservice/src/main/java/nl/kantilever/replayservice/domain/EenEvent.java
@@ -1,34 +1,81 @@
 package nl.kantilever.replayservice.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EenEvent {
 
-  private Integer Id;
-  private String CorrelationId;
+  private Integer id;
+  private Long timestamp;
+  private String correlationId;
+  private String routingKey;
+  private String eventType;
+  private String jsonMessage;
 
+  @JsonProperty("Id")
   public Integer getId() {
-    return Id;
+    return id;
   }
 
   public void setId(Integer id) {
-    Id = id;
+    this.id = id;
   }
 
+  @JsonProperty("CorrelationId")
   public String getCorrelationId() {
-    return CorrelationId;
+    return correlationId;
   }
 
   public void setCorrelationId(String correlationId) {
-    CorrelationId = correlationId;
+    this.correlationId = correlationId;
+  }
+
+  @JsonProperty("Timestamp")
+  public Long getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(Long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  @JsonProperty("RoutingKey")
+  public String getRoutingKey() {
+    return routingKey;
+  }
+
+  public void setRoutingKey(String routingKey) {
+    this.routingKey = routingKey;
+  }
+
+  @JsonProperty("EventType")
+  public String getEventType() {
+    return eventType;
+  }
+
+  public void setEventType(String eventType) {
+    this.eventType = eventType;
+  }
+
+  @JsonProperty("JsonMessage")
+  public String getJsonMessage() {
+    return jsonMessage;
+  }
+
+  public void setJsonMessage(String jsonMessage) {
+    this.jsonMessage = jsonMessage;
   }
 
   @Override
   public String toString() {
     return "EenEvent{" +
-      "Id=" + Id +
-      ", CorrelationId='" + CorrelationId + '\'' +
+      "id=" + id +
+      ", timestamp=" + timestamp +
+      ", correlationId='" + correlationId + '\'' +
+      ", routingKey='" + routingKey + '\'' +
+      ", eventType='" + eventType + '\'' +
+      ", jsonMessage='" + jsonMessage + '\'' +
       '}';
   }
 }
