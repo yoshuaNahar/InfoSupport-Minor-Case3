8ac26b3fb0feb8db2a0cc70e651e2a9e4c30718e
Jimmy Nguyen <s1087315@student.hsleiden.nl>
2018-01-15 12:41:38 +0100

    some login stuff

--- front-end-webwinkel/src/environment.js
+++ front-end-webwinkel/src/environment.js
@@ -1,6 +1,7 @@
 let environment = {
   API: {
     webwinkel: "http://localhost:8081",
-    bestellingservice: "http://localhost:8082"
+    bestellingservice: "http://localhost:8082",
+    gebruikerservice: "http://localhost:9999"
   }
 };
