c9ff57cdae3ca01981381f206a4a0a537996826b
Jimmy Nguyen <s1087315@student.hsleiden.nl>
2018-01-17 15:19:02 +0100

    noah fucked up

--- front-end-webwinkel/src/environment.prod.js
+++ front-end-webwinkel/src/environment.prod.js
@@ -2,6 +2,6 @@ let environment = {
   API: {
     webwinkel: "http://192.168.178.158:8926",
     bestellingservice: "http://l92.168.178.158:8930",
-    accountservice: "http://l92.168.178.158:8937
+    accountservice: "http://l92.168.178.158:8937"
   }
 };
