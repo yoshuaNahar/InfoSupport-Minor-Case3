8c9607694d66430e3a86f825068baf0c3186ed59
Yoshua Nahar <yosh.nahar@gmail.com>
2018-01-17 14:26:07 +0100

    add tests for replayservice

--- replayservice/src/main/java/nl/kantilever/replayservice/config/AppConfig.java
+++ replayservice/src/main/java/nl/kantilever/replayservice/config/AppConfig.java
@@ -1,11 +1,11 @@
 package nl.kantilever.replayservice.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Component
-public class config {
+@Configuration
+public class AppConfig {
 
   @Bean
   public RestTemplate simpleRestTemplate(){
