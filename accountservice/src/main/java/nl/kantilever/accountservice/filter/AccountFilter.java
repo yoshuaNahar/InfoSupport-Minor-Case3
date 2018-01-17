b2bc01d25f549b534133f28b5596defd671e15a6
NoahKr <noahkroon1997@gmail.com>
2018-01-17 16:37:42 +0100

    fix de shizzle

--- accountservice/src/main/java/nl/kantilever/accountservice/filter/AccountFilter.java
+++ accountservice/src/main/java/nl/kantilever/accountservice/filter/AccountFilter.java
@@ -51,27 +51,28 @@ public class AccountFilter implements Filter {
 
     if (role == null) {
       chain.doFilter(request, response);
-    }
+    } else {
 
-    try {
-      Jws<Claims> claims = Jwts
-        .parser()
-        .setSigningKey(this.accessTokenSecret)
-        .parseClaimsJws(accessToken);
+      try {
+        Jws<Claims> claims = Jwts
+          .parser()
+          .setSigningKey(this.accessTokenSecret)
+          .parseClaimsJws(accessToken);
 
-      if (!claims.getBody().get("role", String.class).equals(role)) {
-        logger.info("role not: {}", role);
+        if (!claims.getBody().get("role", String.class).equals(role)) {
+          logger.info("role not: {}", role);
+          ((HttpServletResponse) response).setStatus(401);
+        } else {
+          logger.info("role is admin, so doFilter, means: go to next Filter or Controller");
+          chain.doFilter(request, response);
+        }
+      } catch (SignatureException e) {
+        logger.info("SignatureException: {}", e);
         ((HttpServletResponse) response).setStatus(401);
-      } else {
-        logger.info("role is admin, so doFilter, means: go to next Filter or Controller");
-        chain.doFilter(request, response);
+      } catch (Exception e) {
+        logger.info("Exception: {}", e);
+        ((HttpServletResponse) response).setStatus(500);
       }
-    } catch (SignatureException e) {
-      logger.info("SignatureException: {}", e);
-      ((HttpServletResponse) response).setStatus(401);
-    } catch (Exception e) {
-      logger.info("Exception: {}", e);
-      ((HttpServletResponse) response).setStatus(500);
     }
   }
 
