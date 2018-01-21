ec3b133ee8254f6f2731b9145c7d188d10202db0
Jimmy Nguyen <s1087315@student.hsleiden.nl>
2018-01-21 17:27:10 +0100

    added centralized login

--- front-end-webwinkel/src/environment.prod.js
+++ front-end-webwinkel/src/environment.prod.js
@@ -3,5 +3,6 @@ let environment = {
     webwinkel: "http://192.168.178.158:8926",
     bestellingservice: "http://l92.168.178.158:8930",
     accountservice: "http://l92.168.178.158:8937"
-  }
+  },
+  role: 'USER'
 };
