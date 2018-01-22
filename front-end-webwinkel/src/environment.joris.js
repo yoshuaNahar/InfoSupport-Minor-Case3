483c075b29b7dda409ff6b958355d0fa4ab582a1
NoahKr <noahkroon1997@gmail.com>
2018-01-22 13:37:11 +0100

    go

---
+++ front-end-webwinkel/src/environment.joris.js
@@ -0,0 +1,8 @@
+let environment = {
+  API: {
+    webwinkel: "http://localhost:8927",
+    bestellingservice: "http://localhost:8930",
+    accountservice: "http://localhost:8937"
+  },
+  role: 'USER'
+};
